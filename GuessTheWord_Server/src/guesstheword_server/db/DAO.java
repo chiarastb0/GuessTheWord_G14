290eab9bb02ba26067c4355fb99110ae2dd62f18
Angel <a.palladino57@studenti.unisa.it>
2026-05-08 12:18:09 +0200

    Ristrutturazione progetto Server

---
+++ GuessTheWord_Server/src/guesstheword_server/db/DAO.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package guesstheword_server.db;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ *
+ * @author angel
+ */
+
+/* Interfaccia per implementare pattern DAO
+   Separa logica di gestione DB dal resto
+*/
+public interface DAO<T>{
+    Optional<T> selectById(int id); //Trova un elemento per ID
+    List<T> selectAll();    //Prende tutti gli elementi
+    void insert(T t);
+    void update(T t);
+    void delete(T t);
+}
