57661e56cb8521d8f69601f2e9d6eb84807b2239
Chiara <c.stabile15@studenti.unisa.it>
2026-05-12 13:50:23 +0200

    driver

--- GuessTheWord_Server/src/guesstheword_server/GuessTheWord_Server.java
+++ GuessTheWord_Server/src/guesstheword_server/GuessTheWord_Server.java
@@ -5,6 +5,7 @@
  */
 package guesstheword_server;
 
+import guesstheword_server.db.DatabaseManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -31,7 +32,7 @@ public class GuessTheWord_Server extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        launch(args);
+        launch(args);     
     }
     
 }
