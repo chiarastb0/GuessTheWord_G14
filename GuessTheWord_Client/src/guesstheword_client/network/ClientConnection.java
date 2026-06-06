99c656ad6bab7d5ef604e0304d00935207a7f573
Chiara <c.stabile15@studenti.unisa.it>
2026-06-06 16:23:59 +0200

    Risolta definitivamente la struttura del progetto Client e Server

--- GuessTheWord_Client/src/guesstheword_client/network/ClientConnection.java
+++ GuessTheWord_Client/src/guesstheword_client/network/ClientConnection.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package guesstheword_client.controller;
+package guesstheword_client.network;
 
 /**
  *
  * @author admin
  */
-public class GameScreenController {
+public class ClientConnection {
     
 }
