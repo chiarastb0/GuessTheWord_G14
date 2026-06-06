6721e7a0b39df54e9d07af39e8bf51efa49c3745
Chiara <c.stabile15@studenti.unisa.it>
2026-06-06 15:09:41 +0200

    Aggiunte classi ClientConnection e GameScreenController

---
+++ src/guessstheword_client/network/ClientConnection.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guessstheword_client.network;
+
+/**
+ *
+ * @author admin
+ */
+
+import guesstheword_client.controller.GameScreenController; 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import javafx.application.Platform;
+
+/**
+ * Gestisce la connessione Socket lato Client e l'ascolto asincrono dei messaggi del Server.
+ */
+public class ClientConnection {
+
+}
--- src/guesstheword_client/controller/GameScreenController.java
+++ src/guesstheword_client/controller/GameScreenController.java
@@ -3,13 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package it.unisa.diem.database;
+package guesstheword_client.controller;
 
 /**
  *
  * @author admin
  */
-public class DatabaseManager {
-    private String string;
+public class GameScreenController {
     
 }
