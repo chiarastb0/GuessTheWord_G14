a2fe5ef071c6b59290ee3ee89a3ee91bbb22a57c
Chiara <c.stabile15@studenti.unisa.it>
2026-06-13 16:17:32 +0200

    modifiche view

--- GuessTheWord_Server/src/guesstheword_server/controller/AdminDashboardController.java
+++ GuessTheWord_Server/src/guesstheword_server/controller/AdminDashboardController.java
@@ -192,7 +192,7 @@ public class AdminDashboardController {
             serverThread.setDaemon(true); 
             serverThread.start();
 
-            lblServerStatus.setText("🟢 In Ascolto...");
+            lblServerStatus.setText("In Ascolto...");
             lblServerStatus.setTextFill(javafx.scene.paint.Color.GREEN);
             btnStartServer.setDisable(true); 
         }
